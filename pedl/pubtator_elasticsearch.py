Fix entity masking

pedl/pubtator_elasticsearch.py
@@ -1,3 +1,4 @@
+import re
 import sys
 from collections import defaultdict
 from operator import itemgetter
@@ -10,7 +11,7 @@ import numpy as np
 from elasticsearch import Elasticsearch, helpers
 from tqdm import tqdm
 
-from pedl.utils import SegtokSentenceSplitter, get_pmid, chunks, cache_root, replace_consistently
+from pedl.utils import SegtokSentenceSplitter, get_pmid, chunks, cache_root, replace_consistently_dict
 from pedl.data_getter import DataGetterAPI
 
 INDEX_NAME = "pubtator_masked"
@@ -20,43 +21,50 @@ def _add_masked_entities(elastic_doc, mask_types=None) -> None:
     elastic_doc["entities_masked"] = {}
     entities = list(elastic_doc["entities"])
 
-    offsets = []
-    lengths_orig = []
-    entity_to_offset_idx = defaultdict(list)
+    span_to_replacement = {}
+
+    idx_mask = 1
+    for entity in entities:
+        spans = elastic_doc["entities"][entity]
+
+        entity_type = entity.split("|")[1]
+        replacement = None
+        if entity_type in mask_types:
+            for span in spans:
+                span = tuple(span)
+                if span in span_to_replacement:
+                    continue
+                else:
+                    if replacement is None:
+                        replacement = "<" + mask_types[entity_type] + str(idx_mask) + "/>"
+                        idx_mask += 1
+                    # fix spans crossing sentence boundaries
+                    if span[1] > len(elastic_doc["text"]):
+                        span = (span[0], len(elastic_doc["text"]))
+
+                    span_to_replacement[span] = replacement
+
+    text = elastic_doc["text"]
+    text_masked, old_span_to_new = replace_consistently_dict(text, span_to_replacement)
+
+    elastic_doc["text_masked"] = text_masked
 
     for entity in entities:
         spans = elastic_doc["entities"][entity]
+        new_spans = []
         for span in spans:
-            entity_to_offset_idx[entity].append(len(offsets))
-            offsets.append(span[0])
-            lengths_orig.append(span[1] - span[0])
+            span = tuple(span)
+            if span in old_span_to_new:
+                new_spans.append(old_span_to_new[span])
+            else:
+                new_spans.append(span)
+        elastic_doc["entities_masked"][entity] = new_spans
 
-    idx_mask = 1
-    text = elastic_doc["text"]
-    lengths_new = np.array(lengths_orig)
-    offsets_new = np.array(offsets)
-    for entity in entities:
-        entity_type = entity.split("|")[1]
-        if entity_type in mask_types:
-            replacement = "<" + mask_types[entity_type] + str(idx_mask) + "/>"
-            for offset_idx in entity_to_offset_idx[entity]:
-                text, offsets_new = replace_consistently(offset=offsets_new[offset_idx],
-                                     length=lengths_orig[offset_idx],
-                                     replacement=replacement,
-                                     text=text,
-                                     offsets=offsets_new)
-                lengths_new[offset_idx] = len(replacement)
-            idx_mask += 1
 
-    elastic_doc["text_masked"] = text
 
-    for entity, offset_indices in entity_to_offset_idx.items():
-        spans_new = []
-        for offset_idx in offset_indices:
-            start = offsets_new[offset_idx]
-            end = start + lengths_new[offset_idx]
-            spans_new.append([start, end])
-        elastic_doc["entities_masked"][entity] = spans_new
+    # assert len(elastic_doc["entities_masked"]) == len(elastic_doc["entities"])
+    # for entity in elastic_doc["entities_masked"]:
+    #     assert len(elastic_doc["entities_masked"][entity]) >= len(elastic_doc["entities"][entity])
 
 
 def _process_pubtator_files(files: List[Path], q: mp.Queue, mask_types=None, entity_marker: dict = None):
@@ -110,6 +118,9 @@ def _process_pubtator_files(files: List[Path], q: mp.Queue, mask_types=None, ent
                             "span": [sentence.start_pos + passage.offset, sentence.end_pos + passage.offset + passage.offset + passage.offset]
                         }
                         while entity[0][0] < sentence.end_pos:
+                            if entity[0][0] - sentence.start_pos < 0:
+                                continue
+
                             if entity[1].type.lower() in elastic_doc:
                                 elastic_doc[entity[1].type.lower()].append(entity[1].cuid)
                                 span = [entity[0][0] - sentence.start_pos, entity[0][1] - sentence.start_pos]
@@ -178,7 +189,7 @@ def build_index(pubtator_file, n_processes, elastic_search_server, masked_types=
         p.start()
         processes.append(p)
 
-    pbar = tqdm(desc="Building pubtator elasticsearch index", total=len(files))
+    pbar = tqdm(desc="Building pubtator elastic index", total=len(files))
     n_files_processed = 0
     while n_files_processed < len(files):
         elastic_docs = q.get()

pedl/utils.py
@@ -1,4 +1,5 @@
 import gzip
+import itertools
 import json
 import logging
 import os
@@ -307,12 +308,14 @@ def replace_consistently(offset, length, replacement, text, offsets):
     return new_text, new_offsets
 
 
-def insert_consistently(offset, insertion, text, starts, ends):
+def insert_consistently(offset, insertion, text, starts, ends, idx_changed):
     new_text = text[:offset] + insertion + text[offset:]
     new_starts = starts.copy()
     new_ends = ends.copy()
-    new_starts[starts >= offset] += len(insertion)
-    new_ends[ends >= offset] += len(insertion)
+    not_idx_changed_mask = np.ones(len(starts), dtype=bool)
+    not_idx_changed_mask[idx_changed] = False
+    new_starts[(starts >= offset) & not_idx_changed_mask] += len(insertion)
+    new_ends[ends > offset] += len(insertion)
 
     return new_text, new_starts, new_ends
 
@@ -331,20 +334,34 @@ def delete_consistently(from_idx, to_idx , text, starts, ends):
 
 
 def replace_consistently_dict(text, span_to_replacement):
-    offsets = list(span_to_replacement)
-    replacements = [span_to_replacement[i] for i in offsets]
+    old_offset_to_new = {}
+    current_offset = 0
+    new_text = ""
 
-    starts = np.array([i[0] for i in offsets])
-    ends = np.array([i[1] for i in offsets])
+    # Sort spans by starting index
+    sorted_spans = sorted(span_to_replacement.items(), key=lambda x: x[0][0])
 
-    for i, replacement in enumerate(replacements):
-        text, starts, ends = delete_consistently(from_idx=starts[i],
-                                                 to_idx=ends[i], starts=starts,
-                                                 ends=ends, text=text)
-        text, starts, ends = insert_consistently(offset=starts[i], insertion=replacement,
-                                                 starts=starts, ends=ends, text=text)
+    for (start, end), substitution in sorted_spans:
+        # Add the text before the current span
+        new_text += text[current_offset:start]
 
-    return text
+        # Add the substitution and update the new offsets
+        old_offset_to_new[(start, end)] = (len(new_text),  len(substitution) + len(new_text))
+        new_text += substitution
+
+        # Update the current offset
+        current_offset = end
+
+    # Add the remaining text
+    new_text += text[current_offset:]
+
+    # check results
+    for (start, end), (new_start, new_end) in old_offset_to_new.items():
+        assert new_text[new_start:new_end] == span_to_replacement[(start, end)]
+
+    return new_text, old_offset_to_new
+
+
 
 
 def get_homologue_mapping(